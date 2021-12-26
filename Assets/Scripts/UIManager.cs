f8e34af21a32bf47889de999a4b1baab8af51a9f
Daniel <itzdanielmichael@gmail.com>
2021-12-26 14:48:47 -0500

    add Pause and shuffle buttons

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
@@ -9,6 +10,16 @@ public class UIManager : MonoBehaviour
   public GameObject roundOverScreen;
   public GameObject winStars1, winStars2, winStars3;
 
+  private Board theBoard;
+
+  public string levelSelect;
+
+  public GameObject pauseScreen;
+
+  private void Awake()
+  {
+    theBoard = FindObjectOfType<Board>();
+  }
   // Start is called before the first frame update
   void Start()
   {
@@ -20,6 +31,46 @@ public class UIManager : MonoBehaviour
   // Update is called once per frame
   void Update()
   {
+    if (Input.GetKeyDown(KeyCode.Escape))
+    {
+      PauseUnpause();
+    }
+  }
 
+  public void PauseUnpause()
+  {
+    if (!pauseScreen.activeInHierarchy)
+    {
+      pauseScreen.SetActive(true);
+      Time.timeScale = 0f;
+    }
+    else
+    {
+      pauseScreen.SetActive(false);
+      Time.timeScale = 1f;
+    }
+
+  }
+
+  public void ShuffleBoard()
+  {
+    theBoard.ShuffleBoard();
+  }
+
+  public void QuitGame()
+  {
+    Application.Quit();
+  }
+
+  public void GoToLevelSelect()
+  {
+    Time.timeScale = 1f;
+    SceneManager.LoadScene(levelSelect);
+  }
+
+  public void TryAgain()
+  {
+    string currentScene = SceneManager.GetActiveScene().name;
+    SceneManager.LoadScene(currentScene);
   }
 }
